917ae5715a6b436121444816807ea25cbde7f8c1
anus26 <anusraza268gmail.com>
2025-04-13 21:32:11 -0700

    images

--- src/Components/ProductItems/index.jsx
+++ src/Components/ProductItems/index.jsx
@@ -7,17 +7,25 @@ import { FaRegHeart } from "react-icons/fa6";
 import { FaCodeCompare } from "react-icons/fa6";
 import { MdZoomOutMap } from "react-icons/md";
 const ProductItems = () => {
+    const items=[
+        {key:'w2'},
+        {key:'w1'},
+    ]
   return (
     <div className='productitems rounded-md shadow-lg bg-slate-200 overflow-hidden relative
-     borer-2 border-[rgba(0,0,0,0.6)]'>
-<div className='group imagewrapper w-[100%] h-[220px] overflow-hidden rounded-md'>
+     borer-2 border-[rgba(0,0,0,0.1)]'>
+<div className='group  imagewrapper w-full h-[220px] overflow-hidden rounded-md'>
     <Link>
-    <div className='w-[100%] overflow-hidden'>
+    {items.map((item)=>(
 
-<img src="./images/15.jpg" alt="images" className='w-full'/>
-<img src="./images/19.jpg" alt="images" className='w-full left-0 top-0  transition-all duration-500 opacity-0 absolute group-hover:opacity-100'/>
+        <div key={item.key} className='relative w-full h-full overflow-hidden'>
+
+<img src={`./images/${item.key}.jpg`} alt="images" className='w-full h-full object-cover transition-all duration-500'/>
+<img src="./images/w1.jpg" alt="images" className='w-full h-full object-cover left-0 top-0  transition-all duration-500 opacity-0 absolute group-hover:opacity-100'/>
     </div>
+    ))}
     </Link>
+    
 <div className='action flex items-center top-[-200px] right-[5px] z-50 absolute gap-2 flex-col w-[50px] transition-all duration-500 group-hover:top-[15px] opacity-0 group-hover:opacity-100'>
     <Button className='!w-[30px] !h[30px] !min-w-[30px] !rounded-full !bg-white text-black hover:!bg-primary hover:text-white group '>
          <MdZoomOutMap className='text-[18px]  !text-black  group-hover:text-white' /></Button>
