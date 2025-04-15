a66f0dcc0ef5596697eb64acf3b01c0c5c84663a
anus26 <anusraza268gmail.com>
2025-04-14 23:43:53 -0700

    picture

--- src/Components/ProductItems/index.jsx
+++ src/Components/ProductItems/index.jsx
@@ -6,24 +6,49 @@ import Button from '@mui/material/Button';
 import { FaRegHeart } from "react-icons/fa6";
 import { FaCodeCompare } from "react-icons/fa6";
 import { MdZoomOutMap } from "react-icons/md";
-const ProductItems = () => {
-    const items=[
-        {key:'w2'},
-        {key:'w1'},
-    ]
+const ProductItems = ({item}) => {
+    // const items=[
+    //     {
+    //         id: 1,
+    //         defaultImg: 'w1',
+    //         hoverImg: 'w2',
+    //         title: 'Men Opaque Casual Shirt',
+    //         brand: 'CLAFOUTIS',
+    //         oldPrice: 58.0,
+
+    //         newPrice: 50.0,
+    //         rating: 4,
+    //       },
+    //     //   {
+    //     //     id: 2,
+    //     //     defaultImg: 'x1',
+    //     //     hoverImg: 'x2',
+    //     //     title: 'Classic Fit Denim Jacket',
+    //     //     brand: 'URBAN THREAD',
+    //     //     oldPrice: 120.0,
+    //     //     newPrice: 95.5,
+    //     //     rating: 5,
+    //     //   },
+         
+    // ]
+   
   return (
-    <div className='productitems rounded-md shadow-lg bg-slate-200 overflow-hidden relative
-     borer-2 border-[rgba(0,0,0,0.1)]'>
+   
+<div className=''>
+
+    {/* {items.map((item)=>( */}
+      <div key={item.id}   className='productitems rounded-md shadow-lg bg-slate-200 overflow-hidden relative
+      borer-2 border-[rgba(0,0,0,0.1)]'>
 <div className='group  imagewrapper w-full h-[220px] overflow-hidden rounded-md'>
     <Link>
-    {items.map((item)=>(
 
-        <div key={item.key} className='relative w-full h-full overflow-hidden'>
+        <div   className='relative w-full h-full overflow-hidden'>
 
-<img src={`./images/${item.key}.jpg`} alt="images" className='w-full h-full object-cover transition-all duration-500'/>
-<img src="./images/w1.jpg" alt="images" className='w-full h-full object-cover left-0 top-0  transition-all duration-500 opacity-0 absolute group-hover:opacity-100'/>
+<img src={`./images/${item.defaultImg}.jpg`} alt="images" className='w-full h-full object-cover transition-all duration-500'/>
+
+
+<img src={`./images/${item.hoverImg}.jpg`}  alt="images" className='w-full h-full object-cover left-0 top-0  transition-all duration-500 opacity-0 absolute group-hover:opacity-100'/>
     </div>
-    ))}
     </Link>
     
 <div className='action flex items-center top-[-200px] right-[5px] z-50 absolute gap-2 flex-col w-[50px] transition-all duration-500 group-hover:top-[15px] opacity-0 group-hover:opacity-100'>
@@ -39,22 +64,26 @@ const ProductItems = () => {
 <div className='info p-3'>
     <h6 className='text-[13px]'>
         <Link to='/'  className='link transition-all '>
-        CLAFOUTIS
+        {item.brand}
         </Link>
     </h6>
     <h3 className='text-[14px] title mt-1 font-[500] mb-1 text-[rgba(0,0,0,0.6)]'>
 
  <Link to='/'  className='link transition-all '>
- Men Opaque Casual Shirt
+ {item.title}
         </Link>
     </h3>
     <Rating name="size-small" defaultValue={2} size="small" readOnly/>
     <div className='flex items-center gap-4'>
-       <span className='oldprice line-through text-gray-500 '>$ 58.00</span>
-       <span className='newprice  text-primary font-[500]'>$ 50.00</span>
+       <span className='oldprice line-through text-gray-500 '>${item.oldPrice}</span>
+       <span className='newprice  text-primary font-[500]'>${item.newPrice}</span>
     </div>
 </div>
     </div>
+ 
+
+{/* ))}  */}
+          </div>
   )
 }
 
--- src/Components/ProductsSlider/index.jsx
+++ src/Components/ProductsSlider/index.jsx
@@ -6,22 +6,91 @@ import 'swiper/css/navigation';
 
 import ProductItems from '../ProductItems';
 
-const ProductsSlider = (props) => {
-  // Temporary array to map SwiperSlides — replace with actual products later
-  const dummyProducts = Array.from({ length: 8 });
+const ProductsSlider = ({ itemsPerSlide }) => {
+  const products = [
+    {
+      id: 1,
+      defaultImg: 'w1',
+      hoverImg: 'w2',
+      title: 'Men Luxary Watch ',
+      brand: 'CLAFOUTIS',
+      oldPrice: 58.0,
+      newPrice: 50.0,
+      rating: 4,
+    },
+    {
+      id: 2,
+      defaultImg: 'x2',
+      hoverImg: 'x1',
+      title: 'Footwear for office use ',
+      brand: 'CLAFOUTIS',
+      newPrice: 95.5,
+      rating: 5,
+    },
+    {
+      id: 3,
+      defaultImg: 's2',
+      hoverImg: 's1',
+      title: 'Important T-Shirt',
+      brand: 'MODSTYLE',
+      oldPrice: 45.0,
+      newPrice: 39.99,
+      rating: 3,
+    },
+    {
+        id: 3,
+        defaultImg: 'm2',
+        hoverImg: 'm1',
+        title: 'Smart Mobilephone',
+        brand: 'MODSTYLE',
+        oldPrice: 45.0,
+        newPrice: 39.99,
+        rating: 3,
+      },
+      {
+        id: 3,
+        defaultImg: 'b2',
+        hoverImg: 'b1',
+        title: 'Ladies Hand  bags ',
+        brand: 'MODSTYLE',
+        oldPrice: 45.0,
+        newPrice: 39.99,
+        rating: 3,
+      },
+      {
+        id: 3,
+        defaultImg: 'm5',
+        hoverImg: 'm4',
+        title: 'Fine and Natural ',
+        brand: 'MODSTYLE',
+        oldPrice: 45.0,
+        newPrice: 39.99,
+        rating: 3,
+      },
+      {
+        id: 3,
+        defaultImg: 'j1',
+        hoverImg: 'j3',
+        title: 'Gold Jewellery',
+        brand: 'MODSTYLE',
+        oldPrice: 45.0,
+        newPrice: 39.99,
+        rating: 3,
+      },
+  ];
 
   return (
-    <div className="products-slider p-[25px] -m-[15px]">
+    <div className=" products-slider w-full h-full p-[25px] -m-[15px]">
       <Swiper
-        slidesPerView={props.items}
+        slidesPerView={itemsPerSlide || 6}
         spaceBetween={10}
         navigation={true}
         modules={[Navigation]}
         className="mySwiper"
       >
-        {dummyProducts.map((_, index) => (
-          <SwiperSlide key={index}>
-            <ProductItems />
+        {products.map((product) => (
+          <SwiperSlide key={product.id}>
+            <ProductItems item={product} />
           </SwiperSlide>
         ))}
       </Swiper>
